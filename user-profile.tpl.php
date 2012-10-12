2772b2d22839ba34d831cbc72a988a05dcbba0c5
nbboob <nbboob@gmail.com>
2012-10-12 16:55:19 +0200

    some improvement

--- user-profile.tpl.php
+++ user-profile.tpl.php
@@ -3,11 +3,11 @@
   <div class='clear'></div>
 </div>
 
-<h2 class="sub-title">History of contributions</h2>
+<h2 class="sub-title" id="history">History of contributions</h2>
 <div class="user-history">
 <div style="padding-left: 20px;">
-<p><a href="<?php print url('/user-history/'.$elements['#account']->name);?>" style="font-weight: bold;">Submissions (<?php print $historyNo['post']?>)</a></p>
-<p><a href="<?php print url('/user-comments/'.$elements['#account']->name);?>" style="font-weight: bold;">Comments (<?php print $historyNo['comment']?>)</a></p>
-<p><a href="<?php print url('/blog/'.$elements['#account']->uid);?>" style="font-weight: bold;">Blogs (<?php print $historyNo['blog']?>)</a></p>
+<p><a href="<?php print url('user-history/'.$elements['#account']->name);?>" style="font-weight: bold;">Submissions (<?php print $historyNo['post']?>)</a></p>
+<p><a href="<?php print url('user-comments/'.$elements['#account']->name);?>" style="font-weight: bold;">Comments (<?php print $historyNo['comment']?>)</a></p>
+<p><a href="<?php print url('blogs/'.$elements['#account']->name);?>" style="font-weight: bold;">Blogs (<?php print $historyNo['blog']?>)</a></p>
 </div>
 </div>
